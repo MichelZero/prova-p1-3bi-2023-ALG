ea19d48ed059f1acfc3d2b9e3989ff74377dc327
Michel <106674127+MichelZero@users.noreply.github.com>
2023-08-31 09:51:03 -0300

    first commit

---
+++ ex01.py
@@ -0,0 +1,16 @@
+#
+# autores: Cristiano e Michel
+
+# 1 - ler uma lista de 5 número reais e imprimir a lista na ordem inversa. (dica use o método “reverse()” )
+
+# entrada de dados
+lista = []
+for i in range(5):
+    lista.append(float(input("Digite um número: ")))
+
+# processamento
+lista.reverse()
+
+# saída de dados
+print(f"Lista na ordem inversa: {lista}")
+      
